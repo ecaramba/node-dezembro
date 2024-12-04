34bb686d3a4fb5104082dc97a7627ba4e5258c89
prof edir <ecaramba@gmail.com>
2024-12-03 21:05:06 -0300

    aula 1

---
+++ exercicio1.js
@@ -0,0 +1,9 @@
+// 1 - inverter o texto
+// Oi Turma -> amruT iO
+let texto = "Que dia ensolarado!";
+
+// 2 - contar vogais e consoantes
+// texto -> vogais: 2 consoantes: 3
+
+// 3 - colocar o texto em ordem alfabetica
+// texto -> eottx
---
+++ ola.js
@@ -0,0 +1,30 @@
+
+console.log("Oi Turma!!!");
+
+// imprimir na tela num de 1 - 10
+
+var ini = 1;
+var fim = 10;
+
+while (ini <= 10)
+{
+    console.log(ini);
+    //ini = ini + 1;
+    // ini += 1;
+    ini++;
+
+}
+
+// usando for
+for (var ini = 1; ini <= 10; ini++)
+{
+    console.log(ini);
+}
+
+
+const nome = "edir"; // null
+// nome = "paulo"; // string
+
+//let nome = "edir"; //
+
+console.log(nome);
