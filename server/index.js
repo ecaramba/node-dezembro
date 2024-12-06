5efc55e792dc076799431f4374701acf2ae4636c
prof edir <ecaramba@gmail.com>
2024-12-06 20:37:53 -0300

    lendo csv

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+const fs = require("fs");
+const csv = require("node-csv").createParser();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -16,8 +19,38 @@ app.get("/contato", function(req, res){
 
 app.post("/contato", function(req,res){
 
-    res.json(req.body);
-    // res.send("form enviado")
+    // res.json(req.body);
+    let dados = req.body.nome 
+            + ","
+            + req.body.cidade 
+            + "," 
+            + req.body.senha 
+            + "\n";
+
+    fs.writeFile("dados.csv", dados, {flag: "a"}, function(){
+        res.send("dados salvos com sucesso");
+    })
+
+});
+
+app.get("/ler", function(req, res){
+    // ler o arquivo csv e imporimir uma tabela
+
+    csv.parseFile("dados.csv", function(err, dados) {
+        console.log(dados);
+        let html = "<table>"
+            + "<tr><th>Nome</th> <th>Cidade</th> </tr>";
+
+        dados.forEach(function(linha){
+            html += "<tr><td>"+ linha[0] +"</td> <td>"+ linha[1] +"</td></tr>"
+
+        });
+        
+        html += "</table>";
+
+        res.send(html)
+    })
+
 });
 
 app.listen(3000, function(){
