d4d998cb69f54e9d5de59ad714525098f273a4e0
prof edir <ecaramba@gmail.com>
2024-12-06 19:36:35 -0300

    post

--- server/index.js
+++ server/index.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
 app.get("/", function (req, res){
     res.send("<html><body><h1>Oi Turma</h1></body></html>");
 });
 
 app.get("/contato", function(req, res){
-    res.status(401).send("Pag Contato");
-})
+    console.log(req.query)
+    res.json(req.query);
+});
+
+app.post("/contato", function(req,res){
+
+    res.json(req.body);
+    // res.send("form enviado")
+});
 
 app.listen(3000, function(){
     console.log("servidor iniciado");
