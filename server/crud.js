8281fc84583d46817ed856597b0646f3666c7ee4
Edir <ecaramba@gmail.com>
2024-12-09 21:54:16 -0300

    bd crud

--- server/crud.js
+++ server/crud.js
@@ -12,19 +12,27 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.post("/cadastro", function(req, res) {
-    res.send("create");
+    //INSERT INTO alunos (nome, email, cidade, idade) VALUES ('roberto', 'rob@abc.com', 'Pinhais', 32);
+    res.json(req.body)
+    // res.send("create");
 });
 
 app.get("/lista", function(req, res) {
+    // SELECT * FROM alunos 
     res.send("read");
 });
 
 app.post("/atualizar", function(req, res){
+    //UPDATE alunos SET nome = "Roberto" WHERE id = 4;
     res.send("update");
 });
 
 app.post("/delete", function(req, res){
+    // DELETE FROM alunos WHERE id = 5;
     res.send("delete");
 });
 
